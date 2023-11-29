609106ec5a457edce7fe9f2e6867e5f25017046d
Jantonio <ja.alcalde.2021@alumnos.urjc.es>
2023-11-29 17:33:25 +0100

    mapa incompleto

---
+++ PRACTICA 2/src/service.js
@@ -0,0 +1,30 @@
+export const dataLabels = ["Nombre", "Year","Color","Motor","Caballos","Opinion"];
+
+//MAPA DE LOS OBJETOS
+let resenas = new Map();
+places.set(0, { Comprador: "Jacinto", Concesionario:"Don Benito", opinion:"", valoracion:""});
+places.set(1, { Comprador: "", Concesionario:"", opinion:"", valoracion:"" });
+places.set(2, { Comprador: "", Concesionario:"", opinion:"", valoracion:"" });
+places.set(3, { Comprador: "", Concesionario:"", opinion:"", valoracion:"" });
+places.set(4, { Comprador: "", Concesionario:"", opinion:"", valoracion:"" });
+places.set(5, { Comprador: "", Conceionario:"", opinion:"", valoracion:"" });
+places.set(6, { Comprador: "", Conceionario:"", opinion:"", valoracion:"" });
+places.set(7, { Comprador: "", Conceionario:"", opinion:"", valoracion:"" });
+places.set(8, { Comprador: "", Conceionario:"", opinion:"", valoracion:"" });
+places.set(9, { Comprador: "", Conceionario:"", opinion:"", valoracion:""});
+places.set(10, { Comprador: "", Conceionario:"", opinion:"", valoracion:"" });
+places.set(11, { Comprador: "", Conceionario:"", opinion:"", valoracion:"" });
+
+let places = new Map();
+places.set(0, { Nombre: 'Seat Ibiza', Year: '2022', Color:'Rojo', Motor: '1.5 TSI', caballos:'150', opinion: resenas.get(0)});
+places.set(1, { Nombre: 'John Deree', Year: '2015', Color:'Verde', Motor: '8000 CC', caballos:'180', opinion: resenas.get(1) });
+places.set(2, { Nombre: 'Citroen C15', Year: '1989', Color:'Gris', Motor: '1.8 TSI', caballos:'65', opinion: resenas.get(2) });
+places.set(3, { Nombre: 'Audi RS7', Year: '2021', Color:'Negro', Motor: '3.0 GTI', caballos:'248', opinion: resenas.get(3) });
+places.set(4, { Nombre: 'Seat 600', Year: '1975', Color:'Rojo', Motor: '1.0 Diesel', caballos:'60', opinion: resenas.get(4) });
+places.set(5, { Nombre: 'Renault F6', Year: '1978', Color:'Blanco', Motor: '1.0 Diesel', caballos:'48', opinion: resenas.get(5) });
+places.set(6, { Nombre: 'Lamborghini Aventador', Year: '2018', Color:'Rosa', Motor: '3.5 TDI', caballos:'650', opinion: resenas.get(6) });
+places.set(7, { Nombre: 'Mario Kart', Year: '2023', Color:'Azul', Motor: '4.0 Biturbo', caballos:'80000', opinion: resenas.get(7) });
+places.set(8, { Nombre: 'Nissan Patrol', Year: '1995', Color:'Gris', Motor: '2.8 Turbo', caballos:'115', opinion: resenas.get(8) });
+places.set(9, { Nombre: 'Citroen C5', Year: '2020', Color:'Gris', Motor: '2.0 TDI', caballos:'90', opinion: resenas.get(9) });
+places.set(10, { Nombre: 'Cupra', Year: '2021', Color:'Gris', Motor: '3.0 GTI', caballos:'300', opinion: resenas.get(10) });
+places.set(11, { Nombre: 'Toyota CHR', Year: '2022', Color:'Blanco', Motor: '1.9 TDI', caballos:'105', opinion: resenas.get(11) });
