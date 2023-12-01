75a88ed26bb5a60b2cd8c852025488194dab6ce9
Jadrados <s.adrados.2020@alumnos.urjc.es>
2023-12-01 17:56:21 +0100

    Cambios para cargar los elementos

--- PRACTICA 2/src/router.js
+++ PRACTICA 2/src/router.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import {coches} from 'service.js'
 //import {dataLabels,data,addPlace,getPlace,delPlace,uptPlace,datalength} from './placesService.js';
 
  export const router = express.Router();
 
 //LA URL QUE SE VA A EJECUTAR SIEMPRE QUE SE CARGUE LA PÁGINA
 router.get('/', (req, res) => {
-    res.render('index');
+    let elementos = coches;
+
+    res.render('index', elementos);
 });
 
 router.get('/form', (req, res) => {
